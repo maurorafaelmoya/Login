b7193e5fa9818ffca93cce7c67b7f319f0f93233
desarrollo3 <desarrollo3@xpd.mx>
2024-08-27 18:00:06 -0600

    initial commit

---
+++ src/views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+    <h2>Iniciar Sesión</h2>
+    <form method="POST">
+        <input type="email" name="email" placeholder="Email" required><br>
+        <br>
+        <input type="password" name="password" placeholder="Contraseña" required><br>
+        <br>
+        <button type="submit">Iniciar Sesión</button>
+        <br>
+        <br>
+        Si no tienes cuenta puedes crear una <a href='/Login/src/views/register.php'>aqui</a>
+    </form>
+</body>
+</html>
---
+++ src/views/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Crear Cuenta</title>
+</head>
+<body>
+    <h2>Crear Cuenta</h2>
+    <form method="POST">
+        <input type="email" name="email" placeholder="Email" required>
+        <br>
+        <br>
+        <input type="password" name="password" placeholder="Contraseña" required>
+        <br>
+        <br>
+        <input type="password" name="password" placeholder="Repetir Contraseña" required>
+        <br>
+        <br>
+        <button type="submit">Crear Cuenta</button>
+    </form>
+</body>
+</html>
